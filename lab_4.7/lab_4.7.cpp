c97c9a598ad2367b5369d3913bea2f77bf69bc23
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-28 10:25:33 +0300

    commit1_master

---
+++ lab_4.7/lab_4.7.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double xp, xk, x, dx,eps , a = 0, R = 0, S = 0;
+	int n = 1;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+	cout << "-----------------------------------------" << endl;
+	cout << "|" << setw(5)  << "x"          << "   |"
+		        << setw(10) <<"asin(x)"   << " |"
+		        << setw(7)  << "S"          << "    |"
+		        << setw(5)  << "n"          << " |"
+		        << endl;
+	cout << "-----------------------------------------" << endl;
+	x = xp; 
+	while (x <= xk)
+	{
+		n = 1; 
+		a = (x * x * x) / 6 ; 
+		S = a;
+		do {
+			n++;
+			R = (( 1.0 * (2 * n - 1) / (2 * n)) * ( 1.0 * (2 * n - 1) / (2 * n + 1)) * (x * x)) ;
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		
+
+         cout << "|" << setw(7) << setprecision(2) << x       << " |"
+			        << setw(10) << setprecision(5) << asin(x) << " |"
+			        << setw(10) << setprecision(5) << x+S      << " |"
+				    << setw(5) << n                          << " |"
+			        << endl;
+		x += dx;
+		cout << "-----------------------------------------" << endl;
+	}
+		return 0;
+}
+
